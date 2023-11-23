373111d300c3bb526cf9916d36637a5588b20982
zayedibnibrahim <zayed.ibn@gmail.com>
2023-11-24 03:19:56 +0600

    deploy to cyclic

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import config from './app/config'
-import mongoose from 'mongoose'
-import app from './app'
+import config from './app/config';
+import mongoose from 'mongoose';
+import app from './app';
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
+    await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
-    })
+      console.log(`Example app listening on port ${config.port}`);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
-main()
+main();
